5f6ad3e27df797363629e70f9dbf9cb7d0799902
vishu221b <vishal.dogra738@gmail.com>
2024-07-10 22:10:32 +1000

    Gsap: fix hero animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,6 @@ import HeroIllustration from "../assets/resume-scan-il.svg";
 import FramrDiv from "./framr/FramrDiv";
 import { useEffect, useRef, useState } from "react";
 import { SignedIn, SignedOut } from "@clerk/clerk-react";
-import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ExpoScaleEase, SlowMo } from "gsap/EasePack";
 
@@ -20,6 +19,7 @@ import { EaselPlugin } from "gsap/EaselPlugin";
 import { PixiPlugin } from "gsap/PixiPlugin";
 import { TextPlugin } from "gsap/TextPlugin";
 import { motion } from "framer-motion";
+import gsap from "gsap";
 
 
 gsap.registerPlugin(useGSAP, Flip, ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin, EaselPlugin, PixiPlugin, TextPlugin, ExpoScaleEase, SlowMo);
@@ -28,6 +28,8 @@ export const Hero = () => {
     const elRef = useRef()
 
     useGSAP(() => {
+        const shape2 = 'M469.539032,263.986786H-0.000001L0,229.890961c310.649475,58.156982,255.61113-98.5,469.539032-65.062302V263.986786z'
+        const shape3 = 'M469.539032,263.986786H-0.000001L0,0c226.11113,0,182.887283-0.414484,469.539032,0V263.986786zz'
         gsap.fromTo(
             '#fullpara',
             {
@@ -57,18 +59,40 @@ export const Hero = () => {
         y: 0,
         delay: 0.4
     })
+
     const [screenW, setScreenW] = useState(0)
     useEffect(() => {
         setScreenW(window.innerHeight)
-    })
+    }, [])
+    // const wave = document.querySelector('#wave')
+    // const logo = document.querySelector('.logo')
+
+
+    // gsap.from(logo, {
+    //     repeat: -1,
+    //     repeatDelay: 1
+    // })
+    // gsap.to(wave, .8, {
+    //     attr: { d: shape2 },
+    //     ease: Power2.easeIn
+    // })
+    // gsap.to(wave, .8, {
+    //     attr: { d: shape3 },
+    //     ease: Power2.easeOut,
+    //     fill: '#77aeff'
+    // })
+    // gsap.from(logo, .8, {
+    //     y: 75
+    // })
     return (
         <div className="rounded-3xl border-0 grid grid-cols-1 md:grid-cols-2 sm:max-w-[90%] mx-auto p-0 sm:p-7">
+            <div id="wave" className="absolute"></div>
             <FramrDiv className="">
                 <motion.img
                     src={HeroIllustration}
                     className={`h-full w-full scale-150 z-50 overflow-visible`}
                     alt=""
-                    animate={{ scale: [0.5, 1.2, 1.2], x: [-screenW / 3, 0] }}
+                    animate={{ scale: [1.2, 1.2, 1.2], x: [-screenW / -7.3, 0], opacity: [0, 1] }}
                     transition={{ duration: 1.2, delay: .4 }}
                 />
             </FramrDiv>
@@ -83,9 +107,6 @@ export const Hero = () => {
                     overflow-hidden
                     "
             >
-                {/* <div>
-                    <Badge variant="outline">We&apos;re live!</Badge>
-                </div> */}
                 <div className="flex gap-4 flex-col border-0">
                     <h1 className={`text-5xl md:text-5xl max-w-lg tracking-tighter text-left font-regular font-["Zen Maru Gothic"]zzz font-medium`}>
                         Build a <span className="text-primary">Professional Resume</span> using AI!
